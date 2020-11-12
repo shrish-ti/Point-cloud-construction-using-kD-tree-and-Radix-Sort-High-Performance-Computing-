90108f1b3b428221899505c3d01e68775301b7d4
shrish-ti <53407836+shrish-ti@users.noreply.github.com>
2020-11-12 15:14:06 +0530

    Update improvise radix.cpp

--- improvise radix.cpp
+++ improvise radix.cpp
@@ -3,9 +3,9 @@
 #define MAX_DIM 2
 using namespace std; 
 
-int getMax(int d_array[][MAX_DIM], int numberOfElements, int dim, int idx) 
+int getMax(int h_array[][MAX_DIM], int numberOfElements, int dim, int idx) 
 {   
-	int mx = d_array[0][idx]; 
+	int mx = h_array[0][idx]; 
 	for (int i = 0; i < numberOfElements; i++) 
 		if (d_array[i][idx] > mx) 
 			mx = d_array[i][idx]; 
@@ -13,47 +13,47 @@ int getMax(int d_array[][MAX_DIM], int numberOfElements, int dim, int idx)
 	
 } 
 
-void countSort(int d_array[][MAX_DIM], int numberOfElements, int dim, int idx, int exp) 
+void countSort(int h_array[][MAX_DIM], int numberOfElements, int dim, int idx, int exp) 
 { 
 	int output[numberOfElements][dim]; 
 	int i, count[10] = { 0 }; 
 
 	for (i = 0; i < numberOfElements; i++) 
-		count[(d_array[i][idx] / exp) % 10]++; 
+		count[(h_array[i][idx] / exp) % 10]++; 
 
 	for (i = 1; i < 10; i++) 
 		count[i] += count[i - 1]; 
 
 	for (i = numberOfElements - 1; i >= 0; i--) { 
-		output[count[(d_array[i][idx] / exp) % 10] - 1][idx] = d_array[i][idx];
+		output[count[(h_array[i][idx] / exp) % 10] - 1][idx] = h_array[i][idx];
 		if(idx==0)
-		output[count[(d_array[i][idx] / exp) % 10] - 1][1] = d_array[i][1];
+		output[count[(h_array[i][idx] / exp) % 10] - 1][1] = h_array[i][1];
 		else if(idx==1)
-		output[count[(d_array[i][idx] / exp) % 10] - 1][0] = d_array[i][0];
-		count[(d_array[i][idx] / exp) % 10]--; 
+		output[count[(h_array[i][idx] / exp) % 10] - 1][0] = h_array[i][0];
+		count[(h_array[i][idx] / exp) % 10]--; 
 	} 
 
 	for (i = 0; i < numberOfElements; i++) 
 	{
-		d_array[i][0] = output[i][0]; 
-		d_array[i][1]=output[i][1];
+		h_array[i][0] = output[i][0]; 
+		h_array[i][1]=output[i][1];
 }
     
 } 
 
-void radixsort(int d_array[][MAX_DIM], int numberOfElements, int dim, int idx) 
+void radixsort(int h_array[][MAX_DIM], int numberOfElements, int dim, int idx) 
 { 
-	int m = getMax(d_array, numberOfElements, dim, idx); 
+	int m = getMax(h_array, numberOfElements, dim, idx); 
 
 	for (int exp = 1; m / exp > 0; exp *= 10) 
-		countSort(d_array, numberOfElements, dim, idx, exp); 
+		countSort(h_array, numberOfElements, dim, idx, exp); 
 } 
 
-void print(int d_array[][MAX_DIM], int numberOfElements, int dim, int idx) 
+/*void print(int h_array[][MAX_DIM], int numberOfElements, int dim, int idx) 
 { 
 	for (int i = 0; i < numberOfElements; i++) 
-		cout << d_array[i][0] << " "<< d_array[i][1] << " "<<endl; 
-} 
+		cout << h_array[i][0] << " "<< d_array[i][1] << " "<<endl; 
+} */
 
 int main() 
 { 
